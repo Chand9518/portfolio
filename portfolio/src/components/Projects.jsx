9f70daae4896b7014561c4616430467406860a39
Chand9518 <93931598+Chand9518@users.noreply.github.com>
2026-03-10 23:47:09 +0530

    Revise project titles and enhance descriptions
    Updated project descriptions and titles to reflect recent work and technologies used, including integrations and migrations.

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -13,12 +13,17 @@ Projects
 <div className="bg-white/5 p-6 rounded-xl hover:scale-105 transition">
 
 <h3 className="text-xl font-semibold">
-Headless AEM CMS
+DXP - Sobeys
 </h3>
 
 <p className="text-gray-400 mt-3">
-Implemented headless architecture delivering content across
-web and apps.
+ Integrated multiple third-party APIs using GraphQL to enable seamless data retrieval and improved application performance.
+</p>
+<p className="text-gray-400 mt-3">
+ Implemented Algolia Search integration to deliver fast, relevant, and scalable search experiences across the application.
+</p>
+<p className="text-gray-400 mt-3">
+ Developed and executed comprehensive Unit Test cases to ensure code quality, reliability, and regression-free deployments.
 </p>
 
 </div>
@@ -26,11 +31,18 @@ web and apps.
 <div className="bg-white/5 p-6 rounded-xl hover:scale-105 transition">
 
 <h3 className="text-xl font-semibold">
-Algolia Search Integration
+Hitachi Vantara - COE
 </h3>
 
 <p className="text-gray-400 mt-3">
-Fast enterprise search integration with AEM Cloud.
+ Led the successful migration of AEM 6.5 to AEM as a Cloud Service, ensuring seamless transition and system integrity
+</p>
+<p className="text-gray-400 mt-3">
+ Integrated Single Sign-On (SSO) authentication with AEM as a Cloud Service, enhancing security and user experience. 
+</p>
+<p className="text-gray-400 mt-3">
+ Engineered and maintained AEM custom components, functionalities, including models, servlets, services, and configurations for dynamic content 
+management.
 </p>
 
 </div>
@@ -38,13 +50,21 @@ Fast enterprise search integration with AEM Cloud.
 <div className="bg-white/5 p-6 rounded-xl hover:scale-105 transition">
 
 <h3 className="text-xl font-semibold">
-React + AEM Components
+Redtag
 </h3>
 
 <p className="text-gray-400 mt-3">
-React based frontend integrated with AEM backend.
+ Developed scalable AEM backend APIs for the RedTag mobile app, delivering travel deals (flights, vacations, hotels, cars) via Solr and JCR.
 </p>
-
+<p className="text-gray-400 mt-3">
+  Built headless GraphQL APIs using AEM Content Fragments for seamless content delivery.
+</p>
+<p className="text-gray-400 mt-3">
+ Deprecated legacy packages (e.g., Apache Cocoon) and implemented compliant alternatives to ensure AEM Cloud Service compatibility
+</p className="text-gray-400 mt-3">
+<p>
+  Developed new AEM components and enhanced legacy components to support evolving UI/UX requirements.
+</p>
 </div>
 
 </div>
